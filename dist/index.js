12513cf4e79bf6df9e0603b868554e6de99742ec
jeff schulz <jeff.schulz@usc.edu>
2021-12-07 22:30:37 -0800

    ...

--- dist/index.js
+++ dist/index.js
@@ -62,18 +62,21 @@ const core = __nccwpck_require__(450);
 const github = __nccwpck_require__(177);
 
 try {
-  // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput("who-to-greet");
   console.log(`Hello ${nameToGreet}!`);
+
   const time = new Date().toTimeString();
   core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
+
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
 
+  const branch = core.getInput("branch");
+  console.log(branch);
+
   const config = {
     build: {
-      branch: core.getInput("branch"),
+      branch: branch,
     },
   };
   const appconfig = JSON.stringify(config);
--- index.js
+++ index.js
@@ -2,18 +2,21 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 try {
-  // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput("who-to-greet");
   console.log(`Hello ${nameToGreet}!`);
+
   const time = new Date().toTimeString();
   core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
+
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
 
+  const branch = core.getInput("branch");
+  console.log(branch);
+
   const config = {
     build: {
-      branch: core.getInput("branch"),
+      branch: branch,
     },
   };
   const appconfig = JSON.stringify(config);
