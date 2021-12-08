db403eeb5da246c64c42ed10ee07065d0f4d6c27
jeff schulz <jeff.schulz@usc.edu>
2021-12-07 23:17:28 -0800

    ...

--- dist/index.js
+++ dist/index.js
@@ -8449,20 +8449,30 @@ try {
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(payload);
 
+  const action = core.getInput("action");
+  console.log(action);
+
   const branch = core.getInput("branch");
   console.log(branch);
 
   const hash = core.getInput("hash");
   console.log(hash);
 
+  const time = github.context.payload.repository.pushed_at;
+  console.log(time);
+
+  const author = core.getInput("author");
+  console.log(author);
+
   const appconfig = {
     build: {
+      action: action,
       tag: "",
       branch: branch,
       hash: hash,
-      time: "",
+      time: time,
       date: "",
-      author: "",
+      author: author,
     },
   };
   const config = JSON.stringify(appconfig);
