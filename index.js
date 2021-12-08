7863507b232d652df0fe23e41de8316d678915ed
jeff schulz <jeff.schulz@usc.edu>
2021-12-07 22:19:50 -0800

    ...

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
+
+  const config = {
+    build: {
+      branch: core.getInput("branch"),
+    },
+  };
+  const appconfig = JSON.stringify(config);
+  core.setOutput("appconfig", appconfig);
 } catch (error) {
   core.setFailed(error.message);
 }
